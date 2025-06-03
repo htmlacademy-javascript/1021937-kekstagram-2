2283c096ca9a08b249c43b83897a518af6fed982
sergey.pavlov <force.space@rambler.ru>
2025-06-04 00:22:59 +0300

    module8-task1 Добавил рендер попапа

--- js/init-popup.js
+++ js/init-popup.js
@@ -1,6 +1,7 @@
 import { photoObjects } from './mock.js';
 import { isKeyDown } from './util';
 
+const body = document.body;
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
@@ -8,7 +9,8 @@ const closePopupButton = bigPicture.querySelector('.big-picture__cancel');
 const commentsCaption = bigPicture.querySelector('.social__caption');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentsListElement = bigPicture.querySelector('.social__comment');
-const body = document.body;
+const commentsCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoaderButton = bigPicture.querySelector('.social__comments-loader');
 
 const POPUP_SERVICE_CLASSES = {
   'HIDDEN': 'hidden',
@@ -65,7 +67,7 @@ const getPhotoComments = (data) => {
     const clonedCommentTemplate = commentsListElement.cloneNode(true);
 
     clonedCommentTemplate.querySelector('.social__picture').src = element.avatar;
-    clonedCommentTemplate.querySelector('.social__picture').alt = element.description;
+    clonedCommentTemplate.querySelector('.social__picture').alt = element.name;
     clonedCommentTemplate.querySelector('.social__text').textContent = element.message;
 
     commentsFragment.append(clonedCommentTemplate);
@@ -84,11 +86,23 @@ const initPopup = (id) => {
   }
 
   getPhotoComments(currentPicture);
-
-  bigPicture.classList.remove(POPUP_SERVICE_CLASSES.HIDDEN);
-  body.classList.add(POPUP_SERVICE_CLASSES.BODY_INIT_POPUP);
-
   initOpenPopupListeners();
+
+  if (!commentsCount.classList.contains(POPUP_SERVICE_CLASSES.HIDDEN)) {
+    commentsCount.classList.add(POPUP_SERVICE_CLASSES.HIDDEN);
+  }
+
+  if (!commentsLoaderButton.classList.contains(POPUP_SERVICE_CLASSES.HIDDEN)) {
+    commentsLoaderButton.classList.add(POPUP_SERVICE_CLASSES.HIDDEN);
+  }
+
+  if (!body.classList.contains(POPUP_SERVICE_CLASSES.BODY_INIT_POPUP)) {
+    body.classList.add(POPUP_SERVICE_CLASSES.BODY_INIT_POPUP);
+  }
+
+  if (bigPicture.classList.contains(POPUP_SERVICE_CLASSES.HIDDEN)) {
+    bigPicture.classList.remove(POPUP_SERVICE_CLASSES.HIDDEN);
+  }
 };
 
 export const openPopup = () => {
