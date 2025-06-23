1bbda250d7c23a83f3557e872ee82c5105e0e67d
sergey.pavlov <force.space@rambler.ru>
2025-06-23 23:32:09 +0300

    module12-task1 Перламутровые пуговицы

--- js/util.js
+++ js/util.js
@@ -4,5 +4,27 @@ export const POPUP_SERVICE_CLASSES = {
 };
 
 export const isValidStringLength = (string, maxLength) => string.length <= maxLength;
-
 export const isEscKeyDown = (event) => event.key === 'Escape';
+
+export function debounce(callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle(callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
